2df4d787f095841b7e009eb015f543ecd989b456
wallstop <wallstop@wallstopstudios.com>
2026-03-11 10:59:23 -0700

    Remove BOM

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WallstopStudios.DxCommandTerminal.Editor")]
 [assembly: InternalsVisibleTo("WallstopStudios.DxCommandTerminal.Tests.Runtime")]
